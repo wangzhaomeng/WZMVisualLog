9158dc8d5eb84077c452bd94bea9201302a112f3
wang <122589615@qq.com>
2019-07-15 17:11:20 +0800

    Initial commit

---
+++ Example/WZMVisualLog/WZMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WZMAppDelegate.h
+//  WZMVisualLog
+//
+//  Created by wangzhaomeng on 07/15/2019.
+//  Copyright (c) 2019 wangzhaomeng. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WZMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/WZMVisualLog/WZMViewController.h
@@ -0,0 +1,13 @@
+//
+//  WZMViewController.h
+//  WZMVisualLog
+//
+//  Created by wangzhaomeng on 07/15/2019.
+//  Copyright (c) 2019 wangzhaomeng. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WZMViewController : UIViewController
+
+@end
